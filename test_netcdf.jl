40f5826f825c9a19464a5f0da6e022362fab72a5
Jessica Liptak <drjessliptak@gmail.com>
2018-12-10 07:07:53 -0500

    updated test file with new dimension definitions and detailed comments

--- test_netcdf.jl
+++ test_netcdf.jl
@@ -1,21 +1,27 @@
+#test_netcdf.jl
+# This script uses the 'Quickstart' instructions from the 
+# JuliaGeo netCDF packagehttps://github.com/JuliaGeo/NetCDF.jl
+# 
+# Install the netCDF package using the Julia package manager
 import Pkg
-#println("hello world")
 #Pkg.add("NetCDF")
+# load the NetCDF library
 using NetCDF
 println("PROCESSING")
+# create a file with a variable called 'precip'
 filename="testfile.nc"
 varname="precip"
-#This will create the variable called var1 in the file myfile.nc.
 #The attributes defined in the Dict attribs are written to the file and are
-#associated with the newly created variable.
-#The dimensions "x1" and "t" of the variable are called "x1" and "t" in
-#this example. If the dimensions do not exist yet in the file, they will be
-#created. The dimension "x1" will be of length 10 and have the values 11..20,
-#and the dimension "t" will have length 20 and the attribute "units" with
-#the value "s".
-attribs =Dict("units" => "mm/d", "data_min" => 0.0, "data_max" => 87.0)
-atts2 = Dict("units"=>"s")
-nccreate(filename,varname,"x1",11:20, "t", 20, atts2 , atts=attribs)
+# associated with the newly created variable.
+attribs = Dict("units" => "mm/d", "data_min" => 0.0, "data_max" => 87.0)
+# create the file, and define the global dimensions
+# 'x1', as a range of from 11-20, and  't' with units of 's'
+units = Dict("units"=>"s")
+nccreate(filename,varname,"x1",collect(11:20), "t", 20, units , atts=attribs)
 # write data to file
-d=rand(1,30)
+d=rand(10,20)
 ncwrite(d, filename, varname)
+# read the 'precip' data from the file
+precip = ncread(filename, varname)
+
+println(precip)
